99433937c18c7656cca5d1bc6d32d2da415885ee
bushracodes <bushracodes@gmail.com>
2025-01-01 11:28:18 +0600

    changed code structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="pt-10 container mx-auto flex flex-col items-center">
+      <h1 className="pb-5">
+        Next.js Authentication with NextAuth.js and Prisma
+      </h1>
+      <div className="flex gap-5">
+        <Button asChild>
+          <Link href="/login">Login</Link>
+        </Button>
+        <Button asChild>
+          <Link href="/signup">Signup</Link>
+        </Button>
+      </div>
     </main>
   );
 }
